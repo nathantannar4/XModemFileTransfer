36a639bef07770f5834d9ebf789ffd05bd341c3a
osboxes.org <osboxes@osboxes>
2017-11-28 17:52:11 -0800

    Done Part 5

--- Ensc351Part2/Debug/src/Ensc351Part2.d
+++ Ensc351Part2/Debug/src/Ensc351Part2.d
@@ -1,4 +1,27 @@
-src/Ensc351Part2.o: ../src/Ensc351Part2.cpp \
- ../src/RageUtil_CircularBuffer.h
+src/Ensc351Part2.o: ../src/Ensc351Part2.cpp ../src/myIO.h \
+ ../src/SenderX.h ../src/PeerX.h ../src/ReceiverX.h ../src/Medium.h \
+ /home/osboxes/XModemFileTransfer/Ensc351/VNPE.h \
+ /home/osboxes/XModemFileTransfer/Ensc351/VNPE_reenable.h \
+ /home/osboxes/XModemFileTransfer/Ensc351/AtomicCOUT.h \
+ /home/osboxes/XModemFileTransfer/Ensc351/ScopedMutex.h \
+ /home/osboxes/XModemFileTransfer/Ensc351/AtomicConsole.h
 
-../src/RageUtil_CircularBuffer.h:
+../src/myIO.h:
+
+../src/SenderX.h:
+
+../src/PeerX.h:
+
+../src/ReceiverX.h:
+
+../src/Medium.h:
+
+/home/osboxes/XModemFileTransfer/Ensc351/VNPE.h:
+
+/home/osboxes/XModemFileTransfer/Ensc351/VNPE_reenable.h:
+
+/home/osboxes/XModemFileTransfer/Ensc351/AtomicCOUT.h:
+
+/home/osboxes/XModemFileTransfer/Ensc351/ScopedMutex.h:
+
+/home/osboxes/XModemFileTransfer/Ensc351/AtomicConsole.h:
